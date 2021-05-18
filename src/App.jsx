b8de967cec492b65fec5d2031ed6a7a8ab2cb1f3
Tiago-Pratas <prattiago@gmail.com>
2021-05-18 11:39:47 +0200

    CHORE: add eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="app">
-     <div>APP</div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div>APP</div>
+        </div>
+    );
 }
 
 export default App;
