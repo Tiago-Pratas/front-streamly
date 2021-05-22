5d601cfc11dac3e2a3c903c45ddfd59de6cbc8ca
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-05-22 12:11:52 +0200

    FEAT: implement swiper wihtout styles

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Scrollbar} from 'swiper';
+import 'swiper/swiper.scss';
+
+SwiperCore.use([Scrollbar]);
+
+const Carousel = () => {
+    return (
+        <>
+            <h1>Carousel</h1>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                scrollbar={{ draggable: true }}
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
+
+export default Carousel;
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Loading from './Loading/Loading';
 import RegisterForm from './RegisterForm/RegisterForm';
 import LoginForm from './LoginForm/LoginForm';
 import Header from './Header/Header';
+import Carousel from './Carousel/Carousel';
 
 export { 
     RegisterForm,
     LoginForm,
     Loading,
     Header,
+    Carousel
 };
