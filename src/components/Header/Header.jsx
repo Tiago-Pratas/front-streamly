d48816c707154a58a1dade9c3ee7a754f1ae6be5
Manuel Carrión <manuwweb@gmail.com>
2021-05-22 20:56:45 +0200

    FEAT: added icon library and header creation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineSearch, AiOutlineUser } from 'react-icons/ai';
+import './Header.scss';
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className="header background-blue">
+            <Link className="header__logo" to="/">Streamly</Link>
+
             <nav className="header__nav">
-                <Link to="/">Inicio</Link>
-                <Link to="/recomendador">Recomendador</Link>
-                <Link to="/favoritos">Favoritos</Link>
-                <Link to="/blog">Blog</Link>
-                <Link to="registro-iniciar-sesion">Registro / Login</Link>
+                <Link className="header__nav-link" to="/recomendador">Recomendador</Link>
+                <Link className="header__nav-link" to="/favoritos">Favoritos</Link>
+                <Link className="header__nav-link" to="/blog">Blog</Link>
             </nav>
+
+            <div className="header__icons">
+                <Link className="header__icons-i" to="/registro-iniciar-sesion"><AiOutlineUser /></Link>
+                <AiOutlineSearch className="header__icons-i" />
+            </div>
         </header>
     );
 };
