616d1b93525f6d323888319af21afa7b5a57a81b
Manuel Carrión <manuwweb@gmail.com>
2021-05-23 14:27:32 +0200

    FEAT: add top carousel filter

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TopFilter from '../TopFilter/TopFilter';
 import { Link } from 'react-router-dom';
 import { AiOutlineSearch, AiOutlineUser } from 'react-icons/ai';
 import './Header.scss';
@@ -6,20 +7,25 @@ import './Header.scss';
 const Header = () => {
 
     return (
-        <header className="header background-blue">
-            <Link className="header__logo" to="/">Streamly</Link>
+        <>
+            <header className="header background-blue">
+                <Link className="header__logo" to="/">Streamly</Link>
 
-            <nav className="header__nav">
-                <Link className="header__nav-link" to="/recomendador">Recomendador</Link>
-                <Link className="header__nav-link" to="/favoritos">Favoritos</Link>
-                <Link className="header__nav-link" to="/blog">Blog</Link>
-            </nav>
+                <nav className="header__nav">
+                    <Link className="header__nav-link" to="/recomendador">Recomendador</Link>
+                    <Link className="header__nav-link" to="/favoritos">Favoritos</Link>
+                    <Link className="header__nav-link" to="/blog">Blog</Link>
+                </nav>
 
-            <div className="header__icons">
-                <Link className="header__icons-i" to="/registro-iniciar-sesion"><AiOutlineUser /></Link>
-                <AiOutlineSearch className="header__icons-i" />
+                <div className="header__icons">
+                    <Link className="header__icons-i" to="/registro-iniciar-sesion"><AiOutlineUser /></Link>
+                    <AiOutlineSearch className="header__icons-i" />
+                </div>
+            </header>
+            <div>
+                <TopFilter />
             </div>
-        </header>
+        </>
     );
 };
 
