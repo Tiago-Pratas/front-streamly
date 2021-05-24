4cf44cc7a81398bf980536b9af30dec617a4c070
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-05-24 14:35:26 +0200

    MODIFY: rename className

--- src/Pages/RecommenderPage/RecommenderPage.jsx
+++ src/Pages/RecommenderPage/RecommenderPage.jsx
@@ -10,35 +10,35 @@ import {IoPlaySkipForwardOutline} from 'react-icons/io5';
 const RecommenderPage = () => {
     return(
         <div className="recommender-container">
-            <div className="recommender-container__text">
-                <h1 className="recommender-container__title">¿Qué busco?</h1>
+            <div className="recommender__container-text">
+                <h1 className="recommender__container-title">¿Qué busco?</h1>
                 <p>Si te da igual no pulses nada</p>
             </div>
             <div className="recommender__button-icon">
-                <div className="recommender-container__text">
+                <div className="recommender__container-text">
                     <button className="recommender__button icon"> <BsDisplayFill className="recommender__icon"/></button>
-                    <p className="recommender-container__p">Serie</p>
+                    <p className="recommender__container-p">Serie</p>
                 </div>
-                <div className="recommender-container__text">
+                <div className="recommender__container-text">
                     <button className="recommender__button"><BiMovie className="recommender__icon"/></button>
-                    <p className="recommender-container__p">Peli</p>
+                    <p className="recommender__container-p">Peli</p>
                 </div>
             </div>
-            <div className="recommender-container__text">
+            <div className="recommender__container-text">
                 <p>Dime de cuanto <span className="recommender-bold">tiempo</span> dispones</p>
             </div>
             <div className="recommender__button-icon">
-                <div  className="recommender-container__text">
+                <div  className="recommender__container-text">
                     <button className="recommender__button"> <IoPlaySkipForwardOutline className="recommender__icon"/></button>
-                    <p className="recommender-container__p">No tengo prisa</p>
+                    <p className="recommender__container-p">No tengo prisa</p>
                 </div>
-                <div className="recommender-container__text">
+                <div className="recommender__container-text">
                     <button className="recommender__button"> <BsPlay className="recommender__icon"/> </button>
-                    <p className="recommender-container__p">Tengo un rato</p>
+                    <p className="recommender__container-p">Tengo un rato</p>
                 </div>
-                <div className="recommender-container__text">
+                <div className="recommender__container-text">
                     <button className="recommender__button"> <FiFastForward className="recommender__icon"/> </button>
-                    <p className="recommender-container__p">Algo rápido</p>
+                    <p className="recommender__container_p">Algo rápido</p>
                 </div>
             </div>
             <div>
