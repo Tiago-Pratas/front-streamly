0ff6a0152e2bdf9b7f994feb769deafbda5b92ae
Tiago-Pratas <prattiago@gmail.com>
2021-05-25 08:03:23 +0200

    FEAT: change styling for swiper and carousel filter

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Carousel } from '../../components';
-import fclub from '../../img/fclub.jpeg';
 
 const Home = () => {
-    const image = fclub;
     return (
         <div>
-            <h1>Página de inicio</h1>
-            <Carousel  image={image}/>
+            <Carousel />
         </div>
     );
 };
