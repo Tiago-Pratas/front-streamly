3012c54bcef7f3cfc79c8f481564ef20eb9b66c4
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-05-26 10:57:06 +0200

    FEAT: add component recommender3page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -5,6 +5,7 @@ import WelcomePage2 from './WelcomePage2/WelcomePage2';
 import RecommenderPage from './RecommenderPage/RecommenderPage';
 import Recommender1Page from './Recommender1Page/Recommender1Page';
 import Recommender2Page from './Recommender2Page/Recommender2Page';
+import Recommender3Page from './Recommender3Page/Recommender3Page';
 import Details from './Details/Details';
 import FilterProviders from './FilterProviders/FilterProviders';
 
@@ -16,6 +17,7 @@ export {
     RecommenderPage,
     Recommender1Page,
     Recommender2Page,
+    Recommender3Page,
     Details,
     FilterProviders
 };
