0ff0f60ece3857cfd9adb4246998c6217697d2c0
Tiago Pratas <prattiago@gmail.com>
2021-05-27 00:53:35 +0000

    Revert "Merge branch 'api-calls-backup' into 'master'"
    This reverts merge request !27

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,11 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Loading, Header, NavbarBottom, UserSettings } from './components';
-import {
-    WelcomePage,
-    WelcomePage2,
-    Home,
-    Auth,
-    RecommenderPage,
-    Recommender1Page,
-    Recommender2Page,
-    Recommender3Page,
-    Details,
-    FilterProviders,
-} from './pages';
-
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import {
-    getAllProviders,
-    getAllTvShows,
-    getAllMovies,
-    getAllGenres,
-} from './redux/slices/data.slice';
-import { checkSessionAsync } from './redux/slices/user.slice';
-
 import './App.scss';
 
 function App() {
-    const dispatch = useDispatch();
-
-    const filteredProviders = useSelector((state) => state.tmdb.topFilter);
-
-    dispatch(getAllProviders());
-    dispatch(getAllGenres());
-    dispatch(getAllTvShows(filteredProviders));
-    dispatch(getAllMovies(filteredProviders));
-
-
-    useEffect(() => {
-        getUser();
-    }, []);
-
-    const getUser = async () => {
-        dispatch(checkSessionAsync());
-    };
-
     return (
-        <>
-            <BrowserRouter>
-                <Loading />
-                <Header />
-                <main>
-                    <Switch>
-                        <Route path="/" exact={true}>
-                            <Home />
-                        </Route>
-                        {/* Esta ruta no está en el menú, porque es la página de bienvenida */}
-                        <Route path="/bienvenida-step1">
-                            <WelcomePage />
-                        </Route>
-                        {/* Esta ruta no está en el menú, porque es la página de bienvenida */}
-                        <Route exact path="/bienvenida-step2">
-                            <WelcomePage2 />
-                        </Route>
-                        <Route path="/registro-iniciar-sesion">
-                            <Auth />
-                        </Route>
-                        <Route path="/user-settings">
-                            <UserSettings />
-                        </Route>
-                        <Route path="/recommender">
-                            <RecommenderPage />
-                        </Route>
-                        <Route path="/recommender1">
-                            <Recommender1Page />
-                        </Route>
-                        <Route path="/recommender2">
-                            <Recommender2Page />
-                        </Route>
-                        <Route path="/recommender3">
-                            <Recommender3Page />
-                        </Route>
-                        <Route path="/details/:format/:id">
-                            <Details />
-                        </Route>
-                        <Route path="/filter-providers">
-                            <FilterProviders />
-                        </Route>
-                    </Switch>
-                </main>
-                <div className="navbar">
-                    <NavbarBottom />
-                </div>
-            </BrowserRouter>
-        </>
+        <div className="app">
+            <div>APP</div>
+        </div>
     );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { userSlice } from './slices/user.slice';
-import { dataSlice } from './slices/data.slice';
 
-export default configureStore({
+export const store = configureStore({
     reducer: {
-        user: userSlice.reducer,
-        tmdb: dataSlice.reducer,
     },
 });
